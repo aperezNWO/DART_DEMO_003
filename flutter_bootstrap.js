79f7904993d8afcd0349785b212727304fd6f205
Pablo Alejandro Perez Acosta <alejandro.perez.acosta@hotmail.com>
2026-08-10 21:54:12 -0500

    🚀 Deploy on 2026-08-10T21:54:11.125895

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "487226139" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2024551690" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
